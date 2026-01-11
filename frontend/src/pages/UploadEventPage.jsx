1e2eb83234a6aa74be5a981479cf5005baf890e4
Sindhu MD <mdsindhu2020@gmail.com>
2026-01-11 19:29:05 +0530

    Build frontend with correct API base URL

--- frontend/src/pages/UploadEventPage.jsx
+++ frontend/src/pages/UploadEventPage.jsx
@@ -3,7 +3,7 @@ function UploadEventsPage() {
     const formData = new FormData();
     formData.append("file", e.target.files[0]);
 
-    await fetch('${import.meta.env.VITE_API_BASE_URL}/api/upload/upload' ,{
+    await fetch('${import.meta.env.VITE_API_BASE_URL}/api/upload' ,{
       method: "POST",
       body: formData
     });
