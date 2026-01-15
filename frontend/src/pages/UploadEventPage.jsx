1690be5659288d156cc9b7c45f14f27dfa3984e0
Sindhu MD <mdsindhu2020@gmail.com>
2026-01-15 23:31:42 +0530

    fix upload api url

--- frontend/src/pages/UploadEventPage.jsx
+++ frontend/src/pages/UploadEventPage.jsx
@@ -3,13 +3,18 @@ function UploadEventsPage() {
     const formData = new FormData();
     formData.append("file", e.target.files[0]);
 
-    await fetch('${import.meta.env.VITE_API_BASE_URL}/api/upload' ,{
-      method: "POST",
-      body: formData
-    });
+    await fetch(
+      `${import.meta.env.VITE_API_BASE_URL}/api/upload`,
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
 
     alert("Upload successful");
   };
 
   return <input type="file" onChange={handleUpload} />;
 }
+
+export default UploadEventsPage;
