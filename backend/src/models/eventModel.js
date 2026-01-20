0a1f03367bcee4e0314e9091fb86769ef0a15039
Sindhu MD <mdsindhu2020@gmail.com>
2026-01-20 12:51:44 +0530

    fix event models

--- backend/src/models/eventModel.js
+++ backend/src/models/eventModel.js
@@ -1,9 +1,22 @@
-//let events = [];
+import pool from "../db/pool.js";
 
-export const addEvents = async (newEvents) => {
- events.push(...newEvents);
+export const addEvents = async (events) => {
+  const query = `
+    INSERT INTO events (name, gate, tickets, time)
+    VALUES ($1, $2, $3, $4)
+  `;
+
+  for (const event of events) {
+    await pool.query(query, [
+      event.name,     // ✅ from CSV
+      event.gate,     // ✅ from CSV
+      event.tickets,  // ✅ from CSV
+      event.time      // ✅ from CSV
+    ]);
+  }
 };
 
 export const getAllEvents = async () => {
-return events;
+  const result = await pool.query("SELECT * FROM events");
+  return result.rows;
 };
