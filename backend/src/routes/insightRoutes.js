27682be08ec7b0cbc67990ea07ffae9252cc1ea9
Sindhu MD <mdsindhu2020@gmail.com>
2026-01-28 15:25:46 +0530

    fix insight controller

--- backend/src/routes/insightRoutes.js
+++ backend/src/routes/insightRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { generateInsights, getInsights } from "../controllers/insightController.js";
+import {  getInsights } from "../controllers/insightController.js";
 
 const router = express.Router();
 
-router.post("/generate", generateInsights);
+
 router.get("/", getInsights);
 
 export default router;
